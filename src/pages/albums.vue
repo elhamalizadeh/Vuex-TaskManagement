b4f3e388607d04095f6e81d753ad7f265c378e54
elhamalizadeh <alizade4591@gmail.com>
2023-02-25 13:41:19 +0330

    albums.js

--- src/pages/albums.vue
+++ src/pages/albums.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="container">
     <h1>List of albums</h1>
+    <div>{{  albums }}</div>
   </div>
 </template>
 
 <script>
+import { useStore } from "vuex";
+import { computed } from "vue";
 export default {
- name:'albumsPage'
+ name:'albumsPage',
+
+ setup(){
+    const albumStore = useStore();
+    const albums = computed(()=>albumStore.getters["album/Allalbums"]);
+
+    async function getAlbums(){
+        await albumStore.dispatch("album/getAllalbums");
+    }
+    getAlbums();
+
+    return { albums }
+ }
 }
 </script>
-
 <style>
 
 </style>
--- src/store/modules/album.js
+++ src/store/modules/album.js
@@ -1,17 +1,32 @@
+import axios from "axios";
+const albums ={
+    namespaced:true,
 
-const album ={
     state:{
-     album:[]
+         albums:[]
     },
     getters:{
-
+        Allalbums(state){
+            return state.albums
+        }
     },
     mutations:{
-
+        setAllalbums(state, albums){
+            state.albums = albums
+        }
     },
     actions:{
+        async getAllalbums({commit}){
+            try{
+                const response = await axios.get('https://jsonplaceholder.typicode.com/albums');
+                commit('setAllalbums', response.data)
+
+            }catch(error){
+                console.log("error dare")
+            }
+        }
 
     }
 
 }
-export default album;
+export default albums;
